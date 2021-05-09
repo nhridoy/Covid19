82c239d6289f6c6117a1c1d9eff4670351f276dd
hridoyboss12 <75487507+hridoyboss12@users.noreply.github.com>
2021-05-10 01:18:45 +0600

    UI, Prediction and New Scrapped Data

--- learn.py
+++ learn.py
@@ -1,9 +1,11 @@
 import pandas as pd
+import numpy as np
 import geocoder
 from covid import Covid
 from datetime import datetime
 import plotly.express as px
 import requests
+from bs4 import BeautifulSoup
 import urllib.request
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
@@ -37,6 +39,7 @@ def get_country(c="bangladesh"):
 
     # Processing Countries Start
     countries = covid_data.list_countries()
+    countries.remove("world")
     country = pd.DataFrame(countries, columns=["country"])
     return countries, county_name, date, time, covid_data
 
@@ -58,6 +61,35 @@ def covid_processing(country_name=country_name):
     da = dtdb["datee"].values
     ti = dtdb["timee"].values
 
+    ### Web Scraping ###
+    full_page = requests.get("https://www.worldometers.info/coronavirus/")
+    full_page = full_page.content
+    soup = BeautifulSoup(full_page)
+    ### Scrapping World Data ###
+    world_o = soup.find_all("tr", {"class": "total_row_world"})[7]
+    world_recovered_new = world_o.find_all("td")[7].text[1:].replace(',', '')
+    if world_recovered_new == "":
+        world_recovered_new = 0
+    else:
+        world_recovered_new = int(world_o.find_all("td")[7].text[1:].replace(',', ''))
+    ### Scrapping Country Data ###
+    country_o = soup.find_all("table", {"class": "table-hover"})[0]
+    country_o = (country_o.find_all("tr"))
+    counter = 0
+    empty_dataframe = []
+    for n in country_o:
+        if n.find(["nobr", "a", "span"]) != None:
+            empty_dataframe.append([counter, (n.find(["nobr", "a", "span"]).text).upper()])
+        counter += 1
+    c_scapped = pd.DataFrame(empty_dataframe)
+    c_scapped.columns = ["C_ID", "C_Name"]
+    C_ID_Selected = int(c_scapped[c_scapped["C_Name"] == country_name.upper()]["C_ID"])
+    country_recovered_new = country_o[C_ID_Selected].find_all("td")[7].text.replace("+", "").replace(",", "")
+    if country_recovered_new == "":
+        country_recovered_new = 0
+    else:
+        country_recovered_new = int(country_o[C_ID_Selected].find_all("td")[7].text.replace("+", "").replace(",", ""))
+
     world = db[db["location"] == "World"]
     country = db[db["location"] == country_name.capitalize()]
     world_total = world[["location", "date", "total_cases", "total_deaths"]]
@@ -71,11 +103,12 @@ def covid_processing(country_name=country_name):
     last_death = int(last_date["total_deaths"])
 
     ### Getting All Datas ###
-    world_total_confirmed_cases = covid_data.get_total_confirmed_cases()
-    world_total_confirmed_cases_new = world_total_confirmed_cases - last_cases
+    world_total_confirmed_cases = int(world_o.find_all("td")[2].text[:].replace(',', ''))
+    world_total_confirmed_cases_new = int(world_o.find_all("td")[3].text[1:].replace(',', ''))
     world_total_death = covid_data.get_total_deaths()
     world_total_death_new = world_total_death - last_death
     world_total_recovered = covid_data.get_total_recovered()
+    world_recovered_new = world_recovered_new
     world_total_active = covid_data.get_total_active_cases()
     world_total_serious = covid_data.get_status_by_country_name('world')['critical']
 
@@ -83,10 +116,50 @@ def covid_processing(country_name=country_name):
     country_total_cases = country_cases['confirmed']
     country_total_death = country_cases['deaths']
     country_total_recovered = country_cases['recovered']
+    country_recovered_new = country_recovered_new
     country_total_active = country_cases['active']
     country_cases_new = country_cases['new_cases']
     country_death_new = country_cases['new_deaths']
     country_total_serious = country_cases['critical']
+
+    ### Machine Learning ###
+    ### World Prediction ###
+    world_total_m = world[["location", "date", "total_cases", "total_deaths", "new_cases"]]
+    world_total_m.loc[len(world_total_m.index)] = ["World", "", world_total_confirmed_cases, world_total_death,
+                                                   world_total_confirmed_cases_new]
+    world_total_m = pd.DataFrame(world_total_m)
+    world_total_m.insert(0, 'id', range(0, 0 + len(world_total_m)))
+    world_total_m = world_total_m.fillna(0)
+    world_total_m = world_total_m.tail(30)
+    x_world = np.array(world_total_m["id"]).reshape(-1, 1)
+    y_world = np.array(world_total_m["total_cases"]).reshape(-1, 1)
+    poly_world = PolynomialFeatures(degree=2)
+    x_world = poly_world.fit_transform(x_world)
+    reg = LinearRegression()
+    reg.fit(x_world, y_world)
+    accuracy_world = reg.score(x_world, y_world)
+    predict_world = reg.predict(x_world)
+    wc = int(world_total_m.tail(1)["id"])+1
+    world_total_predicted_cases = int(reg.predict(poly_world.fit_transform([[wc]])))
+
+    ### Country Prediction ###
+    country_total_m = country[["location", "date", "total_cases", "total_deaths", "new_cases"]]
+    country_total_m.loc[len(country_total_m.index)] = [country_name, "", country_total_cases, country_total_death,
+                                                       country_cases_new]
+    country_total_m = pd.DataFrame(country_total_m)
+    country_total_m.insert(0, 'id', range(0, 0 + len(country_total_m)))
+    country_total_m = country_total_m.fillna(0)
+    country_total_m = country_total_m.tail(30)
+    x_country = np.array(country_total_m["id"]).reshape(-1, 1)
+    y_country = np.array(country_total_m["new_cases"]).reshape(-1, 1)
+    poly_country = PolynomialFeatures(degree=3)
+    x_country = poly_country.fit_transform(x_country)
+    reg_c = LinearRegression()
+    reg_c.fit(x_country, y_country)
+    accuracy_country = reg_c.score(x_country, y_country)
+    predict_country = reg_c.predict(x_country)
+    cc = int(country_total_m.tail(1)["id"])+1
+    country_total_predicted_cases = int(reg_c.predict(poly_country.fit_transform([[cc]])))
 
     ### Figure Making ###
     world_country = [world_total, country_total]
@@ -95,9 +168,9 @@ def covid_processing(country_name=country_name):
                   title=f'Total Cases Compare Between World and {country_name}', color='location')
     fig = fig.to_html(fig, full_html=False)
 
-    return da, ti, world_total_confirmed_cases, world_total_confirmed_cases_new, world_total_death, world_total_death_new, world_total_recovered, world_total_active, world_total_serious, country_cases, country_total_cases, country_total_death, country_total_recovered, country_total_active, country_cases_new, country_death_new, country_total_serious, fig
+    return da, ti, world_total_confirmed_cases, world_total_confirmed_cases_new, world_total_death, world_total_death_new, world_total_recovered, world_recovered_new, world_total_active, world_total_serious, country_cases, country_total_cases, country_total_death, country_total_recovered, country_recovered_new, country_total_active, country_cases_new, country_death_new, country_total_serious, world_total_predicted_cases, country_total_predicted_cases, fig
 
 
-'''world_total_confirmed_cases, world_total_confirmed_cases_new, world_total_death, world_total_death_new, world_total_recovered, world_total_active, world_total_serious, country_cases, country_total_cases, country_total_death, country_total_recovered, country_total_active, country_cases_new, country_death_new, country_total_serious, fig = covid_processing()
+'''da, ti, world_total_confirmed_cases, world_total_confirmed_cases_new, world_total_death, world_total_death_new, world_total_recovered, world_recovered_new, world_total_active, world_total_serious, country_cases, country_total_cases, country_total_death, country_total_recovered, country_recovered_new, country_total_active, country_cases_new, country_death_new, country_total_serious, world_total_predicted_cases, country_total_predicted_cases, fig = covid_processing()
 
-print(fig)'''
+print(world_total_predicted_cases, country_total_predicted_cases)'''
--- script1.py
+++ script1.py
@@ -8,13 +8,13 @@ app = Flask(__name__)
 def home():
     # return render_template("index.html")
     countries, country_name, date, time, covid_data = learn.get_country()
-    da, ti, world_total_confirmed_cases, world_total_confirmed_cases_new, world_total_death, world_total_death_new, world_total_recovered, world_total_active, world_total_serious, country_cases, country_total_cases, country_total_death, country_total_recovered, country_total_active, country_cases_new, country_death_new, country_total_serious, fig = learn.covid_processing()
+    da, ti, world_total_confirmed_cases, world_total_confirmed_cases_new, world_total_death, world_total_death_new, world_total_recovered, world_recovered_new, world_total_active, world_total_serious, country_cases, country_total_cases, country_total_death, country_total_recovered, country_recovered_new, country_total_active, country_cases_new, country_death_new, country_total_serious, world_total_predicted_cases, country_total_predicted_cases, fig = learn.covid_processing()
     # x = pd.DataFrame(np.random.randn(20, 5))
 
     detected_country = country_name
     if request.args.get("select_country") in countries:
         country_name = request.args.get("select_country")
-        da, ti, world_total_confirmed_cases, world_total_confirmed_cases_new, world_total_death, world_total_death_new, world_total_recovered, world_total_active, world_total_serious, country_cases, country_total_cases, country_total_death, country_total_recovered, country_total_active, country_cases_new, country_death_new, country_total_serious, fig = learn.covid_processing(
+        da, ti, world_total_confirmed_cases, world_total_confirmed_cases_new, world_total_death, world_total_death_new, world_total_recovered, world_recovered_new, world_total_active, world_total_serious, country_cases, country_total_cases, country_total_death, country_total_recovered, country_recovered_new, country_total_active, country_cases_new, country_death_new, country_total_serious, world_total_predicted_cases, country_total_predicted_cases, fig = learn.covid_processing(
             str(country_name))
 
     countries.remove(country_name.lower())
@@ -22,21 +22,26 @@ def home():
     iframe = Markup(fig)
 
     return render_template("index.html", detected_country=detected_country, country=countries,
-                           country_name=country_name, date=date, time=time, da = da, ti = ti,
+                           country_name=country_name, date=date, time=time, da=da, ti=ti,
                            world_total_confirmed_cases=world_total_confirmed_cases,
                            world_total_confirmed_cases_new=world_total_confirmed_cases_new,
                            world_total_death=world_total_death, world_total_death_new=world_total_death_new,
-                           world_total_recovered=world_total_recovered, world_total_active=world_total_active,
+                           world_total_recovered=world_total_recovered, world_recovered_new=world_recovered_new,
+                           world_total_active=world_total_active,
                            world_total_serious=world_total_serious,
                            country_cases=country_cases, country_total_cases=country_total_cases,
                            country_total_death=country_total_death,
-                           country_total_recovered=country_total_recovered, country_total_active=country_total_active,
+                           country_total_recovered=country_total_recovered, country_recovered_new=country_recovered_new,
+                           country_total_active=country_total_active,
                            country_cases_new=country_cases_new, country_death_new=country_death_new,
-                           country_total_serious=country_total_serious, fig=iframe)
+                           country_total_serious=country_total_serious,
+                           world_total_predicted_cases=world_total_predicted_cases,
+                           country_total_predicted_cases=country_total_predicted_cases, fig=iframe)
     '''return f"{simplejson.dumps(country_cases)},Name = {country_name}, Total Case = {country_total_cases}, New Case = {country_cases_new}, Total Death = {country_total_death}, New Death = {country_death_new}, Total Active = {country_total_active}"'''
 
 
 if __name__ == "__main__":
     from waitress import serve
+
     serve(app)
     app.run(debug=True)
