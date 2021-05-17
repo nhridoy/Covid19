f4db6a02d43ac555e011365c78de5521e61f4f33
Mona Lisa <75487507+hridoyboss12@users.noreply.github.com>
2021-05-17 12:26:45 +0600

    sqlite3

--- sqdb.py
+++ sqdb.py
@@ -1,8 +1,7 @@
-import psycopg2
+import sqlite3
 import pandas as pd
 from datetime import datetime
 from dateutil.parser import parse
-from sqlalchemy import create_engine
 
 ### Date Time ###
 dt = datetime.now()
@@ -12,25 +11,18 @@ date = dates.strftime("%Y-%m-%d")
 time = times.strftime('%I:%M %p')
 
 ### Creating Database ###
-Host = 'bpmxjxipzptjyo9bnuuw-postgresql.services.clever-cloud.com'
-Database = 'bpmxjxipzptjyo9bnuuw'
-User = 'ui5ypjqofyk02elenu1p'
-Port = '5432'
-Password = '4xIAdt3KsL3gJDovo92q'
-URI = 'postgresql://ui5ypjqofyk02elenu1p:4xIAdt3KsL3gJDovo92q@bpmxjxipzptjyo9bnuuw-postgresql.services.clever-cloud.com:5432/bpmxjxipzptjyo9bnuuw'
-
-con = psycopg2.connect(f"dbname='{Database}' user='{User}' password='{Password}' host='{Host}' port='{Port}'")
+con = sqlite3.connect("covid-data.db", check_same_thread=False)
 cur = con.cursor()
-engine = create_engine(f'postgresql://{User}:{Password}@{Host}:{Port}/{Database}')
-url = "https://covid.ourworldindata.org/data/owid-covid-data.csv"
 
+### CREATE Date DB ###
+con2 = sqlite3.connect("date.db", check_same_thread=False)
+cur2 = con2.cursor()
 ### CREATE Date TABLE ###
-cur.execute("CREATE TABLE if NOT EXISTS d_t (datee TEXT, timee TEXT)")
-con.commit()
-
+cur2.execute("CREATE TABLE if NOT EXISTS d_t (datee, timee)")
+con2.commit()
 ### VIEW Date DATA ###
-cur.execute("SELECT * from d_t")
-row = cur.fetchall()
+cur2.execute("SELECT * from d_t")
+row = cur2.fetchall()
 old_date = row[0][0]
 old_time = row[0][1]
 old_time = (f"{old_date} {old_time}")
@@ -41,10 +33,10 @@ new_date = parse(str(dates))
 new_time = parse(time)
 print(f"Old Date: {old_date}")
 print(f"New Date: {new_date}")
-print(f"Old Time: {old_time}")
-print(f"New Time: {new_time}")
 
 if old_date == new_date:
+    old_time = datetime.strptime(str(old_time), "%Y-%m-%d %H:%M:%S")
+    new_time = datetime.strptime(str(new_time), "%Y-%m-%d %H:%M:%S")
     comp_time = new_time-old_time
     static_time = datetime.strptime(f"{date} 04:00:00", "%Y-%m-%d %H:%M:%S")
     comp_time = datetime.strptime(f"{date} {comp_time}", "%Y-%m-%d %H:%M:%S")
@@ -64,30 +56,38 @@ def in_date(date, time):
         # url = "owid-covid-data.csv"
         df = pd.read_csv(url)
         col = list(df)
-        df.to_sql("covid", engine, if_exists='replace')
+
+        ### Creating Table ###
+        cur.execute(
+            "CREATE TABLE if NOT EXISTS covid ({},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},"
+            "{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},"
+            "{},{} )".format(*col))
         con.commit()
 
+        ### Intering Values from csv to Table ###
+        cur.execute("DELETE from covid")
+        con.commit()
+        df.to_sql("covid", con, if_exists='append', index=False)
+
+
         ### DELETING Date DATA ###
-        cur.execute("DELETE from d_t")
-        con.commit()
+        cur2.execute("DELETE from d_t")
+        con2.commit()
         ### INSERT Date DATA ###
-        cur.execute("INSERT INTO d_t (datee, timee) VALUES (%s,%s)", (date, time))
-        con.commit()
+        cur2.execute("INSERT INTO d_t (datee, timee) VALUES (?,?)", (date, time))
+        con2.commit()
 
         ### Viewing Table Column Names ###
         '''names = list(map(lambda x: x[0], cur.description))
         print(names)'''
-        con.commit()
+        con2.commit()
         con.commit()
         print("Executed")
 
     ### VIEW Date DATA ###
-    dtdb = pd.read_sql_query("SELECT * FROM d_t", con)
+    dtdb = pd.read_sql_query("SELECT * FROM d_t", con2)
 
     ### VIEW Covid DATA ###
     db = pd.read_sql_query("SELECT * FROM covid", con)
 
     return dtdb, db
-
-
-
